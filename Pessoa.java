e6082e7631664f08dd57de0c3d48fc67dbe5da3d
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-10-04 15:29:27 -0300

    Exercicio criado classe pessoa

---
+++ Pessoa.java
@@ -0,0 +1,39 @@
+public class Pessoa{
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String cpf;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+}
